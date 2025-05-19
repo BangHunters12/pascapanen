a43130973f4c1ac6bccb5cd545de5f7d837139a8
wafi <awafi2527@gmail.com>
2025-05-19 10:21:28 +0700

    api produk

--- routes/api.php
+++ routes/api.php
@@ -5,7 +5,7 @@ use App\Http\Controllers\Api\RegisterController;
 use App\Http\Controllers\Api\BeritaController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\Api\ProdukApiController;
+use App\Http\Controllers\Api\ProdukController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
