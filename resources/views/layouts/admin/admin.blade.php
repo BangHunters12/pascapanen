b4e0e432bbaf3405bd5e319c8fcfd5e23629673f
Reo Arya Yudha_E41231386 <2005jus@gmail.com>
2025-05-24 19:14:14 +0700

    fix: dashboard style

--- resources/views/layouts/admin/admin.blade.php
+++ resources/views/layouts/admin/admin.blade.php
@@ -9,7 +9,7 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@tabler/icons-webfont@latest/tabler-icons.min.css">
 
-    <link rel="stylesheet" href="{{ asset('assets/css/style.css') }}">
+    {{-- <link rel="stylesheet" href="{{ asset('assets/css/style.css') }}"> --}}
 
     @stack('styles')
 </head>
