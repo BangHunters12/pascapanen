8ffd3a83c94155c05419011edc99aca36086d19b
Reo Arya Yudha_E41231386 <2005jus@gmail.com>
2025-05-24 19:24:50 +0700

    fix: sidebar admin

--- resources/views/layouts/admin/sidebar.blade.php
+++ resources/views/layouts/admin/sidebar.blade.php
@@ -3,14 +3,13 @@
         <a href="{{ route('dashboard') }}" class="text-nowrap logo-img">
             <img src="{{ asset('assets/images/logos/dark-logo.svg') }}" width="200" alt="Logo" />
         </a>
-        {{-- <button class="btn btn-link sidebar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#sidebar" aria-expanded="false" aria-controls="sidebar"> --}}
-        <i class="ti ti-menu"></i>
+        <button class="btn btn-link sidebar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#sidebar" aria-expanded="false" aria-controls="sidebar">
+            <i class="ti ti-menu"></i>
         </button>
     </div>
 
     <nav class="sidebar-nav scroll-sidebar" data-simplebar>
         <ul id="sidebarnav" class="nav flex-column">
-
             <!-- Home Section -->
             <li class="nav-small-cap mt-2">
                 <i class="ti ti-dots nav-small-cap-icon fs-4"></i>
@@ -20,114 +19,98 @@
                 <a class="sidebar-link" href="{{ route('dashboard') }}">
                     <i class="ti ti-layout-dashboard"></i> Dashboard
                 </a>
-                </div>
+            </li>
 
-                <nav class="sidebar-nav scroll-sidebar" data-simplebar>
-                    <ul id="sidebarnav" class="nav flex-column">
+            <!-- Management Section -->
+            <li class="nav-small-cap mt-2">
+                <i class="ti ti-dots nav-small-cap-icon fs-4"></i>
+                <span class="hide-menu">Manajemen</span>
+            </li>
 
-                        <!-- Home Section -->
-                        <li class="nav-small-cap mt-2">
-                            <i class="ti ti-dots nav-small-cap-icon fs-4"></i>
-                            <span class="hide-menu">Home</span>
-                        </li>
+            <!-- Farmers Dropdown -->
+            <li class="sidebar-item">
+                <a class="sidebar-link d-flex justify-content-between align-items-center"
+                    data-bs-toggle="collapse" href="#petaniMenu" role="button" aria-expanded="false"
+                    aria-controls="petaniMenu">
+                    <div><i class="ti ti-user"></i> Petani</div>
+                    <i class="ti ti-chevron-down"></i>
+                </a>
+                <div class="collapse" id="petaniMenu">
+                    <ul class="nav flex-column ms-3">
                         <li class="sidebar-item">
-                            <a class="sidebar-link" href="{{ route('dashboard') }}">
-                                <i class="ti ti-layout-dashboard"></i> Dashboard
+                            <a class="sidebar-link" href="{{ route('petani.index') }}">
+                                <i class="ti ti-users"></i> Data Petani
                             </a>
                         </li>
-
-                        <!-- Management Section -->
-                        <li class="nav-small-cap mt-2">
-                            <i class="ti ti-dots nav-small-cap-icon fs-4"></i>
-                            <span class="hide-menu">Manajemen</span>
-                        </li>
-
-                        <!-- Farmers Dropdown -->
                         <li class="sidebar-item">
-                            <a class="sidebar-link d-flex justify-content-between align-items-center"
-                                data-bs-toggle="collapse" href="#petaniMenu" role="button" aria-expanded="false"
-                                aria-controls="petaniMenu">
-                                <div><i class="ti ti-user"></i> Petani</div>
-                                <i class="ti ti-chevron-down"></i>
-                            </a>
-                            <div class="collapse" id="petaniMenu">
-                                <ul class="nav flex-column ms-3">
-                                    <li class="sidebar-item">
-                                        <a class="sidebar-link" href="{{ route('petani.index') }}">
-                                            <i class="ti ti-users"></i> Data Petani
-                                        </a>
-                                    </li>
-                                    <li class="sidebar-item">
-                                        <a class="sidebar-link" href="{{ route('padi.index') }}">
-                                            <i class="ti ti-leaf"></i> Jenis Padi
-                                        </a>
-                                    </li>
-                                </ul>
-                            </div>
-                        </li>
-
-                        <!-- Other Management Items -->
-                        <li class="sidebar-item">
-                            <a class="sidebar-link" href="{{ route('produk.index') }}">
-                                <i class="ti ti-package"></i> Produk
+                            <a class="sidebar-link" href="{{ route('padi.index') }}">
+                                <i class="ti ti-leaf"></i> Jenis Padi
                             </a>
                         </li>
-
-                        <li class="sidebar-item">
-                            <a class="sidebar-link" href="{{ route('jenis-sewa.index') }}">
-                                <i class="ti ti-assembly"></i> Jenis Sewa
-                            </a>
-                        </li>
-
-                        <li class="sidebar-item">
-                            <a class="sidebar-link" href="{{ route('produksi_beras.index') }}">
-                                <i class="ti ti-building"></i> Produksi Beras
-                            </a>
-                        </li>
-
-
-                        <li class="sidebar-item">
-                            <a class="sidebar-link" href="{{ route('pengajuansewa.index') }}">
-                                <i class="ti ti-clipboard-text"></i> Pengajuan Sewa
-                            </a>
-                        </li>
-
-                        <li class="sidebar-item">
-                            <a class="sidebar-link" href="{{ route('pengajuanpadi.index') }}">
-                                <i class="ti ti-clipboard-text"></i> Pengajuan Padi
-                            </a>
-                        </li>
-
-                        <li class="sidebar-item">
-                            <a class="sidebar-link" href="{{ route('berita.index') }}">
-                                <i class="ti ti-news"></i> Berita
-                            </a>
-                        </li>
-
-                        <li class="sidebar-item">
-                            <a class="sidebar-link" href="{{ route('laporan.index') }}">
-                                <i class="ti ti-news"></i> Laporan
-                            </a>
-                        </li>
-
-                        <!-- Distribution Section -->
-                        <li class="nav-small-cap mt-2">
-                            <i class="ti ti-dots nav-small-cap-icon fs-4"></i>
-                            <span class="hide-menu">Distribusi</span>
-                        </li>
-
-                        <!-- Reports Section -->
-                        <li class="nav-small-cap mt-2">
-                            <i class="ti ti-dots nav-small-cap-icon fs-4"></i>
-                            <span class="hide-menu">Laporan</span>
-                        </li>
-
-                        <!-- Settings Section -->
-                        <li class="nav-small-cap mt-2">
-                            <i class="ti ti-dots nav-small-cap-icon fs-4"></i>
-                            <span class="hide-menu">Pengaturan</span>
-                        </li>
-
                     </ul>
-                </nav>
+                </div>
+            </li>
+
+            <!-- Other Management Items -->
+            <li class="sidebar-item">
+                <a class="sidebar-link" href="{{ route('produk.index') }}">
+                    <i class="ti ti-package"></i> Produk
+                </a>
+            </li>
+
+            <li class="sidebar-item">
+                <a class="sidebar-link" href="{{ route('jenis-sewa.index') }}">
+                    <i class="ti ti-assembly"></i> Jenis Sewa
+                </a>
+            </li>
+
+            <li class="sidebar-item">
+                <a class="sidebar-link" href="{{ route('produksi_beras.index') }}">
+                    <i class="ti ti-building"></i> Produksi Beras
+                </a>
+            </li>
+
+            <li class="sidebar-item">
+                <a class="sidebar-link" href="{{ route('pengajuansewa.index') }}">
+                    <i class="ti ti-clipboard-text"></i> Pengajuan Sewa
+                </a>
+            </li>
+
+            <li class="sidebar-item">
+                <a class="sidebar-link" href="{{ route('pengajuanpadi.index') }}">
+                    <i class="ti ti-clipboard-text"></i> Pengajuan Padi
+                </a>
+            </li>
+
+            <li class="sidebar-item">
+                <a class="sidebar-link" href="{{ route('berita.index') }}">
+                    <i class="ti ti-news"></i> Berita
+                </a>
+            </li>
+
+            <li class="sidebar-item">
+                <a class="sidebar-link" href="{{ route('laporan.index') }}">
+                    <i class="ti ti-news"></i> Laporan
+                </a>
+            </li>
+
+            <!-- Distribution Section -->
+            <li class="nav-small-cap mt-2">
+                <i class="ti ti-dots nav-small-cap-icon fs-4"></i>
+                <span class="hide-menu">Distribusi</span>
+            </li>
+
+            <!-- Reports Section -->
+            <li class="nav-small-cap mt-2">
+                <i class="ti ti-dots nav-small-cap-icon fs-4"></i>
+                <span class="hide-menu">Laporan</span>
+            </li>
+
+            <!-- Settings Section -->
+            <li class="nav-small-cap mt-2">
+                <i class="ti ti-dots nav-small-cap-icon fs-4"></i>
+                <span class="hide-menu">Pengaturan</span>
+            </li>
+        </ul>
+    </nav>
 </aside>
